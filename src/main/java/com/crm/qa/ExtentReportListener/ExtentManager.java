a98f2d60b41114284ada2e3fcfc400552837a0df
nmithun82 <nmithun82@gmail.com>
2018-10-25 15:31:18 -0400

    modified the code for reports

--- src/main/java/com/crm/qa/ExtentReportListener/ExtentManager.java
+++ src/main/java/com/crm/qa/ExtentReportListener/ExtentManager.java
@@ -7,13 +7,15 @@ import com.relevantcodes.extentreports.ExtentReports;
 
 
 public class ExtentManager {
+	private static final String Extent = null;
 	private static ExtentReports extent;
 	public static ExtentReports getInstance() {
 		if (extent == null) {
 	        Date d=new Date();
 			String fileName =d.toString().replace(":", "_").replace(" ", "_")+".html";
 			//extent = new ExtentReports("/Users/mithun_pc/Documents/Project_Reports/FreeCRM"+"/"+fileName, true, DisplayOrder.NEWEST_FIRST);	
-			extent = new ExtentReports(System.getProperty("user.dir")+"/"+fileName, true, DisplayOrder.NEWEST_FIRST);	
+			//extent = new ExtentReports(System.getProperty("user.dir")+"/"+fileName, true, DisplayOrder.NEWEST_FIRST);	
+			extent = new ExtentReports(System.getProperty("user.dir") +"/test-output/Extent.html", true, DisplayOrder.NEWEST_FIRST);
 			extent.loadConfig(new File(System.getProperty("user.dir")+"/ReportsConfig.xml"));
 			
 			extent.addSystemInfo("Selenium Version", "3.14.0").addSystemInfo(	"Environment", "QA");
